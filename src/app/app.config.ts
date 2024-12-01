dc27d88eb142810aadb4e8da486a1d010acee5e2
Ricky <ricky2k7@gmail.com>
2024-12-01 18:34:43 +0100

    chore: Add SSR to project for CSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimations(),
+    provideAnimations(), provideClientHydration(withEventReplay()),
   ],
 };
