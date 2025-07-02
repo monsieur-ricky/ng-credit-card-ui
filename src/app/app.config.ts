faf0ab17b3093bd31dd388b8ffcb18c683a42e68
Ricky <ricky2k7@gmail.com>
2025-07-03 00:17:30 +0100

    chore: Update Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,21 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimations(), provideClientHydration(withEventReplay()),
+    provideAnimations(),
+    provideClientHydration(withEventReplay()),
   ],
 };
